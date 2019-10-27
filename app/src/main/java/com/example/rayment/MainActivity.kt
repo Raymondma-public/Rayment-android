31a2e0fa334718f2ed1b888c809371f32691bfa3
Raymondma-public <raymondma.public@gmail.com>
2019-10-27 18:29:57 +0800

    first commit

---
+++ app/src/main/java/com/example/rayment/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.rayment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
